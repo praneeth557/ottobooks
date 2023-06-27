7358eaab80f4aa24cd6114495c5b6eedd2e32f0c
Praneeth Veerlapally <praneeth7516@gmail.com>
2023-06-26 20:06:47 -0400

    updated api url and node version

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,4 @@
 export class Constants {
-    public static API_ENDPOINT = "http://ec2-3-17-63-108.us-east-2.compute.amazonaws.com:5000/";
+  public static API_ENDPOINT =
+    'http://ec2-3-142-245-187.us-east-2.compute.amazonaws.com:5000/';
 }
