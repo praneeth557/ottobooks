bf1fba5a4b098c0a65fec59ce99c54f972767d79
Praneeth Veerlapally <praneeth7516@gmail.com>
2023-06-27 20:14:27 -0400

    updated api base url

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,4 +1,3 @@
 export class Constants {
-  public static API_ENDPOINT =
-    'http://ec2-3-142-245-187.us-east-2.compute.amazonaws.com:5000/';
+  public static API_ENDPOINT = 'https://lilac-backend.onrender.com/';
 }
